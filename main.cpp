b9a435518ba8584d1cb0beba3ad8b0adb232ebf0
JeffSandov6 <jeffry_sandoval24@yahoo.com>
2017-04-05 16:48:35 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
-#include <fstream>
+
 using namespace std;
 
 int main(){
-  
+	ifstream inStream;
+
+	inStream.open("names.txt");
+
+
+
+
+	
+
+	cout << "the sum total of all the name score is: " << endl;
+
+	return 0;
+}
