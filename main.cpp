42d4b239d275240be9ca3b38eb69ec6eff274a32
joemcmahon95 <jlmcm@mac.com>
2017-04-19 17:16:08 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,45 +1,61 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
-#include <string>
 using namespace std;
 
-int main() {
-	long int totalScore = 0, order = 1;
-	string line;
-ifstream names;
-ofstream sorted;
-names.open("names.txt");
-sorted.open("sortednames.txt");
-	
-	string one;
+int main(){
+    
+    string nameArray[]={};
+    string name, nextName, onePass = "start" , swapCount = "swap" , stopSort = "don't stop";
+    int letterNum = 0 , total = 0 , nameTotal = 0;
+    int size = sizeof(nameArray)/sizeof(nameArray[0]);
+    //TODO read from file and stor names in nameArray.
+    
 
-	names >> one;
-	
-//sorting function
-	
-	sorted << one;
-names.close();
-sorted.close();
-	
-ifstream myfile;
-myfile.open("sortednames.txt");
-	while (getline(myfile, line))
-	{
-		int nameScore = 0;
-
-		for (int i = 0; i < line.length(); i++)
-		{	
-			if (isalpha(line[i]))
-			{
-				char letter = line[i];
-				nameScore += (int)letter - 64;
-			}
-		}
-		cout << line << "\t" << nameScore << endl;
-		totalScore += (nameScore*order);
-		order++;
-	}
-	cout << "The sum total of all the name score is: " << totalScore << endl;
-	system("pause");
+    
+    
+    
+    // puts nameArray into alphabetical order
+    while(swapCount == "swap"){
+        swapCount = "stop";
+        
+        for(int wordNum = 0; wordNum < size - 1; wordNum++){
+            onePass = "start";
+            letterNum = 0;
+            
+            while (onePass != "stop"){
+                
+                if(nameArray[wordNum][letterNum] > nameArray[wordNum+1][letterNum]){
+                    swap(nameArray[wordNum],nameArray[wordNum+1]);
+                    onePass= "stop";
+                    swapCount = "swap";}
+                
+                else if(nameArray[wordNum][letterNum] == nameArray[wordNum+1][letterNum]){
+                    letterNum++;}
+                
+                else{
+                    onePass="stop";}
+            }
+        }
+    }
+    
+    // changes names in list to numbers and creates a total
+    for(int i = 0; i < size; i++){
+        name = nameArray[i];
+        
+        for(int j = 0; j < name.length(); j++){
+            nameTotal = nameTotal + (name[j]-64); }
+        
+        nameTotal = nameTotal * i;
+        total = total + nameTotal;
+        nameTotal = 0;
+    }
+    
+    //prints out final list
+    for(int i = 0; i < size; i++){
+        cout<< nameArray[i]<<endl;}
+    
+    //prints out total number of list
+    cout << "the total for all names is " << total << endl;
+    
+    return 0;
 }
